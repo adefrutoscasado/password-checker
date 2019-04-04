97a9623dff9e1dbffd847862b9594453b5f6ee0b
Natalia Martin <natalia.martin@perseivivarium.com>
2019-04-04 14:07:19 +0200

    add redux reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,34 @@
-import {combineReducers} from 'redux';
-import login, {name as loginName} from '../components/login/reducer';
+import {DO_LOGIN_REQUEST, DO_LOGIN_COMMIT, DO_LOGIN_ROLLBACK} from '../components/login/action-types';
 
-const appReducer = combineReducers({
-  [loginName]: login,
-  routerFluxFocus: (state = { routerFluxFocus: undefined }) => state //Fake reducer, we need to hook in the rootReducer to intercept RNRF events and set current focus scene
-});
-
-const rootReducer = (state, action) => {
-  try {
-    console.log('ACTION', action);
-    return appReducer(state, action);
-  } catch (e) {
-    console.log('REDUX ERROR', e)
-  }
+export const initialState = {
+  loggedIn: false,
+  loginError: '',
+  userId: null,
+  access_token: null
 };
 
-export default rootReducer;
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case DO_LOGIN_REQUEST:
+      return state;
+
+    case DO_LOGIN_COMMIT:
+      return {
+        ...state,
+        loggedIn: true,
+        userId: action.payload.id,
+        access_token: action.payload.access_token
+      }
+    
+    case DO_LOGIN_ROLLBACK:
+      return {
+        ...initialState,
+        loggedIn: false,
+        loginError: action.payload.message
+      }
+    
+    default:
+      return state;
+  }
+}
